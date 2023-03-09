b2c79de3e8efd4774413c84e0b51a3718deb1708
rioto.nagano <kingdom950927@gmail.com>
2023-03-10 07:08:09 +0900

    ボタン押下時にテキスト変化

--- src/components/modal/ModalForm.jsx
+++ src/components/modal/ModalForm.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Field } from 'redux-form'
 import renderField from '../renderField'
+import { buttonTextChange } from '../../modules/buttonText'
 
 const ModalForm = () => {
 
   const buttonText = useSelector(state => state.buttonText)
+  const dispatch = useDispatch();
+
+  // ボタンクリック後にテキスト変化
+  const buttonClick = () => {
+    switch(buttonText.text) {
+    case '最初のテキスト':
+      dispatch(buttonTextChange('2回目のテキスト内容に変更'))
+      break
+    case '2回目のテキスト内容に変更':
+      dispatch(buttonTextChange('最初のテキスト'))
+      break
+    default:
+      break
+    }
+  }
 
   return (
     <>
       <div>Modalフォームです。</div>
       <Field name="modalform" component={renderField} />
       <br></br>
-      <button>{buttonText.text}</button>
+      <button onClick={buttonClick}>{buttonText.text}</button>
       <br></br>
     </>
   )
--- src/modules/buttonText/index.js
+++ src/modules/buttonText/index.js
@@ -6,17 +6,19 @@ const initialState = {
 
 // reducer
 export const buttonText = (state = initialState, action) => {
+
   switch(action.type) {
     case 'TEXT_CHANGE':
-      return { ...state, }
+      return { ...state, text: action.payload }
     default:
       return state
   }
 }
 
 // Action Creator
-export const buttonTextChange = () => {
+export const buttonTextChange = (payload) => {
   return {
-    type:  'TEXT_CHANGE'
+    type:  'TEXT_CHANGE',
+    payload: payload
   }
 }
