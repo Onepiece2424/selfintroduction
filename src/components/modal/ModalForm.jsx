3072db5de43ec7eae29fbfc404e5fee7b2db3f48
rioto.nagano <kingdom950927@gmail.com>
2023-03-12 17:43:28 +0900

    不要な部分をコメントアウトし、Paper componentでモーダルをスタイリング

--- src/components/modal/ModalForm.jsx
+++ src/components/modal/ModalForm.jsx
@@ -1,35 +1,43 @@
+import { Paper } from '@material-ui/core'
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+// import { useSelector, useDispatch } from 'react-redux'
 import { Field } from 'redux-form'
 import renderField from '../renderField'
-import { buttonTextChange } from '../../modules/buttonText'
+// import { buttonTextChange } from '../../modules/buttonText'
 
 const ModalForm = () => {
 
-  const buttonText = useSelector(state => state.buttonText)
-  const dispatch = useDispatch();
+  // const buttonText = useSelector(state => state.buttonText)
+  // const dispatch = useDispatch();
 
   // ボタンクリック後にテキスト変化
-  const buttonClick = () => {
-    switch(buttonText.text) {
-    case '最初のテキスト':
-      dispatch(buttonTextChange('2回目のテキスト内容に変更'))
-      break
-    case '2回目のテキスト内容に変更':
-      dispatch(buttonTextChange('最初のテキスト'))
-      break
-    default:
-      break
-    }
-  }
+  // const buttonClick = () => {
+  //   switch(buttonText.text) {
+  //   case '最初のテキスト':
+  //     dispatch(buttonTextChange('2回目のテキスト内容に変更'))
+  //     break
+  //   case '2回目のテキスト内容に変更':
+  //     dispatch(buttonTextChange('最初のテキスト'))
+  //     break
+  //   default:
+  //     break
+  //   }
+  // }
 
   return (
     <>
-      <div>Modalフォームです。</div>
-      <Field name="modalform" component={renderField} />
-      <br></br>
-      <button onClick={buttonClick}>{buttonText.text}</button>
-      <br></br>
+      <Paper elevation={12} className="modalContent">
+        <br></br>
+        <div>Modalフォームです。</div>
+        <Field name="modalform" component={renderField} />
+        <br></br>
+        <Field name="form1" component={renderField} label="form1" />
+        <Field name="form2" component={renderField} label="form2" />
+        <Field name="form3" component={renderField} label="form3" />
+        <Field name="form4" component={renderField} label="form4" />
+        {/* <button onClick={buttonClick}>{buttonText.text}</button> */}
+        <br></br>
+      </Paper>
     </>
   )
 }
