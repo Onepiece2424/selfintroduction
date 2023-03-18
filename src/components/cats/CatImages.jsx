a27080648b0eb6b63a04040b8f45e22192712144
rioto.nagano <kingdom950927@gmail.com>
2023-03-18 15:29:45 +0900

    他の猫の画像も取得するためのボタンを作成

--- src/components/cats/CatImages.jsx
+++ src/components/cats/CatImages.jsx
@@ -21,10 +21,20 @@ const CatImages = () => {
     });
   }, []);
 
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+
   return (
     <>
       <div>猫の画像</div>
       <div>{loading || <img src={imageUrl} alt="猫の画像です。" width="300px" />}</div>
+      <br></br>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
     </>
   )
 }
